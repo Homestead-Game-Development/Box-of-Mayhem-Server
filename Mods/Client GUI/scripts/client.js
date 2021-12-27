854582d176f01112802b962f5ea6a95340d7302b
rater <rater193@gmail.com>
2021-12-27 06:31:40 +0100

    Moved block placement/breaking over to a mod

--- Mods/Client GUI/scripts/client.js
+++ Mods/Client GUI/scripts/client.js
@@ -2,3 +2,49 @@ Events.register("onServerStart", function() {
     ClientCache.AddScript(__dirname+"/client/Overlay.js");
     ClientCache.AddScript(__dirname+"/client/PlayerControls.js");
 })
+
+
+//Net.Register(messageID, triggeredFunction)
+Net.Register(200,function(reader) {
+    let mousebtn = reader.readInt();
+    let x = reader.readInt();
+    let y = reader.readInt();
+    let z = reader.readInt();
+    let data = null;
+
+    switch(mousebtn) {
+        case 0://left
+        console.log(`Leftclick: ${x}, ${y}, ${z}`);
+        worldengine.worlds.overworld.SetBlock(Math.floor(x), Math.floor(z), Math.floor(y),-1);
+        worldengine.worlds.overworld.UpdateChunk(Math.floor(x/16),Math.floor(z/16),Math.floor(y/16));
+        worldengine.worlds.overworld.SaveChunk(Math.floor(x/16),Math.floor(z/16),Math.floor(y/16));
+        //Here we are telling all other clients we broke the block
+        data = bufferWriter();
+        data.writeInt(1);
+        data.writeInt(x);
+        data.writeInt(y);
+        data.writeInt(z);
+        Net.FireAllClients(201, data);
+        break;
+
+        case 1://right
+        console.log(`Rightclick: ${x}, ${y}, ${z}`);
+        worldengine.worlds.overworld.SetBlock(Math.floor(x), Math.floor(z), Math.floor(y),1);
+        worldengine.worlds.overworld.UpdateChunk(Math.floor(x/16),Math.floor(z/16),Math.floor(y/16));
+        worldengine.worlds.overworld.SaveChunk(Math.floor(x/16),Math.floor(z/16),Math.floor(y/16));
+        //Here we are telling all other clients we placed the block
+        data = bufferWriter();
+        data.writeInt(0);
+        data.writeInt(x);
+        data.writeInt(y);
+        data.writeInt(z);
+        data.writeInt(2);//blockID
+        Net.FireAllClients(201, data);
+
+        break;
+
+        case 2://middle
+        console.log(`Middleclick: ${x}, ${y}, ${z}`);
+        break;
+    }
+});
--- Mods/Client GUI/scripts/client/PlayerControls.js
+++ Mods/Client GUI/scripts/client/PlayerControls.js
@@ -1,6 +1,13 @@
 let camX = 0;
 let camY = 0;
 
+let txtPosition = null;
+events.Register("onClientStart", function() {
+    txtPosition = new GUI.Text("Test");
+    txtPosition.SetSize(512,24);
+    txtPosition.SetPosition(8, 512)
+});
+
 
 events.Register("onClientUpdate", function() {
     if(Input.GetKeyDown(KeyCode.Tab))
@@ -18,10 +25,70 @@ events.Register("onClientUpdate", function() {
         camY = (camY > 89) ? 89 : (camY < -89) ? -89 : camY;
         LocalPlayer.SetCameraRotation(camX, camY);
     }
+    if(txtPosition) {
+        //let playerpos = LocalPlayer.GetPosition();
+        //txtPosition.SetText(`Pos: ${Math.floor(0)}, ${Math.floor(0)}, ${Math.floor(0)}`);
+        let playerpos = LocalPlayer.GetPosition();
+        txtPosition.SetText("Pos: " + Math.floor(playerpos.x) + ", " + Math.floor(playerpos.y) + ", " + Math.floor(playerpos.z));
+    }
 });
 
+events.Register("onClientLateUpdate", function() {
 
-events.Register("onClientStart", function() {
-    console.log(LocalPlayer.GetPosition());
-    //SomeUnhandledFunction();
+    if(Input.GetMouseButtonDown(0)) {//Left click
+        let data = new BufferData();
+        data.WriteInt(0);//write that you are left clicking
+        data.WriteInt(Math.floor(BlockPosition.x));
+        data.WriteInt(Math.floor(BlockPosition.y));
+        data.WriteInt(Math.floor(BlockPosition.z));
+        Net.Send(200, data);
+        console.log("Left: " + BlockPosition.ToString());
+        LocalPlayerWorld.SetBlock(Math.floor(BlockPosition.x), Math.floor(BlockPosition.y), Math.floor(BlockPosition.z), -1);
+    }
+    if(Input.GetMouseButtonDown(1)) {//Right click
+        let data = new BufferData();
+        data.WriteInt(1);//write that you are right clicking
+        data.WriteInt(Math.floor(BlockPlacePosition.x));
+        data.WriteInt(Math.floor(BlockPlacePosition.y));
+        data.WriteInt(Math.floor(BlockPlacePosition.z));
+        Net.Send(200, data);
+        console.log("Right: " + BlockPlacePosition.ToString());
+        
+        LocalPlayerWorld.SetBlock(Math.floor(BlockPlacePosition.x), Math.floor(BlockPlacePosition.y), Math.floor(BlockPlacePosition.z), 1);
+        
+    }
+    if(Input.GetMouseButtonDown(2)) {//Middle click
+        let data = new BufferData();
+        data.WriteInt(2);//write that you are middle clicking
+        data.WriteInt(Math.floor(BlockPosition.x));
+        data.WriteInt(Math.floor(BlockPosition.y));
+        data.WriteInt(Math.floor(BlockPosition.z));
+        Net.Send(200, data);
+        console.log("Middle: " + BlockPosition.ToString());
+    }
+    /*
+    let playerpos = LocalPlayer.GetPosition();
+    txtPosition.SetText(`Pos: ${Math.floor(playerpos.x)}, ${Math.floor(playerpos.y)}, ${Math.floor(playerpos.z)}`)*/
+});
+
+Net.Register(201,function(reader) {
+    let action = reader.ReadInt();
+    let x = reader.ReadInt();
+    let y = reader.ReadInt();
+    let z = reader.ReadInt();
+    let blockID = -1;
+    switch(action) {
+        case 0://Place
+            blockID = reader.ReadInt();
+            LocalPlayerWorld.SetBlock(x, y, z, blockID);
+        break;
+
+        case 1://Break
+            LocalPlayerWorld.SetBlock(x, y, z, -1);
+        break;
+
+        default:
+            console.log("<color=yellow>Unhandled block place action: " + action + "</color>");
+        break;
+    }
 });
